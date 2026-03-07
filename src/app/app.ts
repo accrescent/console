22fd5a8a15f9f4834717293f0e3dd9b3d1820c36
lberrymage <logan@lberrymage.dev>
2026-03-07 02:47:44 +0000

    Use indent width of 4

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,11 @@ import { Component, signal } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: "acc-root",
-  imports: [RouterOutlet],
-  templateUrl: "./app.html",
-  styleUrl: "./app.css",
+    selector: "acc-root",
+    imports: [RouterOutlet],
+    templateUrl: "./app.html",
+    styleUrl: "./app.css",
 })
 export class App {
-  protected readonly title = signal("console");
+    protected readonly title = signal("console");
 }
